a2ab879b4b88e452d75fe4a1a8f03e9263f53754
edwinjoju <92308660+edwinjoju@users.noreply.github.com>
2023-05-01 08:13:02 +0000

    set manifest

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for, jsonify
 from datetime import datetime
+from verify_email import verify_email
 import pymysql.cursors
 import os
 
